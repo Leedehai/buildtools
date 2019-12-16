7026aa3fad1fa342e2f55ca9f3c06a81d7f1bfef
Leedehai <leedehai@outlook.com>
2019-12-16 12:29:35 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,41 @@
+# buildtools
+
+Build tools: [ninja](https://ninja-build.org) and [GN](https://gn.googlesource.com/gn)
+
+### Prerequisites
+
+- Linux or macOS
+- Python 2.7+ or 3.5+
+
+### Transparent setup
+
+Ninja and GN are niche, unlike Make and CMake, which are almost ubiquitous on every
+developer's machine. Therefore, the proxy scripts [ninja.py](ninja.py) and [gn.py](gn.py)
+will download the binaries over the network on first invocation if:
+- `ninja` and `gn` are missing from `PATH`, or
+- `ninja` and `gn` are found on `PATH`, but `gclient` is also found on `PATH` (presence
+of `gclient` implies the `ninja` and `gn` commands found are customized for the Chromium projects).
+
+### Alternative setup
+
+If you wish not to download Ninja and GN binaries over the network or there's an error,
+you can tell the proxy scripts to use locally-built/installed binaries.
+
+```sh
+# Assume you are at this directory.
+
+# step 1: create bin/XXX and change working directory
+mkdir -p bin/linux # on macOS: mkdir -p bin/darwin
+cd bin/linux       # on macOS: cd bin/darwin
+
+# step 2: make symlinks
+ln -s /path/to/your/gn gn
+ls -s /path/to/your/ninja ninja
+
+# step 3: verify
+cd ../..
+./gn.py --version
+./ninja.py --version
+```
+
+###### EOF
---
+++ gn.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# Copyright: see README and LICENSE under the project root directory.
+# Author: @Leedehai
+#
+# File: gn.py
+# ---------------------------
+# Wrapper script of the GN binary. If binary is not found, it will
+# download it.
+
+import os, sys
+import platform
+import subprocess
+
+def get_platform():
+    if platform.machine() not in ["AMD64", "x86_64"]:
+        return None
+    if sys.platform.startswith("linux"):
+        return "linux-amd64"
+    if sys.platform == "darwin":
+        return "mac-amd64"
+    raise NotImplementedError("Platform '%s' not supported" % sys.platform)
+
+BIN_DIR = os.path.join(
+    os.path.abspath(os.path.dirname(__file__)), "bin", (
+        "linux" if get_platform().startswith("linux") else "darwin"))
+BIN_PATH = os.path.join(BIN_DIR, "gn")
+
+def execute_with_downloaded_bin(args):
+    if not os.path.isfile(BIN_PATH):
+        if 0 != subprocess.call([
+            os.path.join(os.path.dirname(os.path.dirname(BIN_DIR)), "get-binary.py") ]):
+            return 1
+    return subprocess.call([ BIN_PATH ] + args[1:])
+
+def has_bin_locally(name):
+    # I could use shutil.which() but it's only available in Python3.3+
+    with open(os.devnull, 'w') as devnull:
+        has_chromium_dev_depot_tools = 0 == subprocess.call([ "which", "gclient" ], stdout=devnull, stderr=devnull)
+        if has_chromium_dev_depot_tools:
+            return False
+        ret = subprocess.call([ "which" ] + [ name ], stdout=devnull, stderr=devnull)
+    return ret == 0
+
+def main(args):
+    programe_name = os.path.basename(BIN_PATH)
+    if has_bin_locally(programe_name):
+        return subprocess.call([ programe_name ] + args[1:])
+    else:
+        return execute_with_downloaded_bin(args)
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main(sys.argv))
+    except KeyboardInterrupt:
+        sys.stderr.write("\x1b[33m gn.py: Interrupted\x1b[0m")
+        sys.exit(1)
